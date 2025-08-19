93e9244fe4713e6aaa364442a2de56ec6a5f10f7
MohammedHieba <mohamedhieba9574@gmail.com>
2025-08-19 14:39:51 +0300

    add api gateway module and define routes.

--- product-service/src/main/java/com/microservices/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservices/product_service/ProductServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Eur
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
