0e4d3958ec49b87b3652ccad41e2493403ff3924
MohammedHieba <mohamedhieba9574@gmail.com>
2025-08-22 04:15:41 +0300

    bypassing security on order service with enabling oauth2 client features, configure credentials order service as oauth2 client.

--- order-service/src/main/java/com/microservices/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/microservices/order_service/OrderServiceApplication.java
@@ -3,7 +3,12 @@ package com.microservices.order_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+				org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration.class,
+				org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration.class
+		})
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- order-service/src/main/java/com/microservices/order_service/configuration/WebClientConfiguration.java
+++ order-service/src/main/java/com/microservices/order_service/configuration/WebClientConfiguration.java
@@ -1,17 +1,72 @@
 package com.microservices.order_service.configuration;
 
+import io.netty.handler.logging.LogLevel;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @Configuration
 public class WebClientConfiguration {
+    @Bean
+    public AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository) {
+
+        // In-memory service لتخزين الـ Access Tokens
+        InMemoryOAuth2AuthorizedClientService clientService =
+                new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
+
+        AuthorizedClientServiceOAuth2AuthorizedClientManager manager =
+                new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, clientService);
+
+        // configure provider لدعم client_credentials
+        OAuth2AuthorizedClientProvider provider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+        manager.setAuthorizedClientProvider(provider);
+
+        return manager;
+    }
 
     @Bean
     @LoadBalanced
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    public WebClient.Builder webClientBuilder(ClientRegistrationRepository clientRegistrationRepository) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+                        authorizedClientManager(clientRegistrationRepository));
+
+        HttpClient httpClient = HttpClient.create()
+                .wiretap("reactor.netty.http.client.HttpClient",
+                        LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL);
+        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient))
+                .apply(oauth2.oauth2Configuration());
+    }
+
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration clientRegistration = ClientRegistration
+                .withRegistrationId("my-client")
+                .clientId("spring-client-credentials-id")
+                .clientSecret("XLtRGriJqiaOH8OsQvNxoBZcKSHf7aRP")
+                .tokenUri("http://localhost:8181/realms/spring-microservices-realm/protocol/openid-connect/token")
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .build();
+
+        return new InMemoryClientRegistrationRepository(clientRegistration);
     }
 
 }
