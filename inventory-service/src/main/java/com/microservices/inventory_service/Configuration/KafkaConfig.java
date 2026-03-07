caab3799f0c85358f723e546c92ce1afd09ad942
MohamedHieba <mohamedhieba9574@gmail.com>
2026-03-07 21:48:59 +0300

    prevent double serialization when forwarding events to dead letter topic  Configured KafkaTemplate with StringSerializer to ensure events are published to DLT without JSON escaping (backslashes).

--- inventory-service/src/main/java/com/microservices/inventory_service/Configuration/KafkaConfig.java
+++ inventory-service/src/main/java/com/microservices/inventory_service/Configuration/KafkaConfig.java
@@ -1,20 +1,38 @@
 package com.microservices.inventory_service.Configuration;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 @Slf4j
 public class KafkaConfig {
-
     @Bean
-    public DefaultErrorHandler defaultErrorHandler(KafkaTemplate<String, Object> kafkaTemplate) {
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+    @Bean
+    public DefaultErrorHandler errorHandler(KafkaTemplate<String, String> kafkaTemplate) {
         DeadLetterPublishingRecoverer recoverer =
                 new DeadLetterPublishingRecoverer(
                         kafkaTemplate,
@@ -24,10 +42,6 @@ public class KafkaConfig {
                         )
                 );
         FixedBackOff backOff = new FixedBackOff(2000L, 3);
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler(recoverer, backOff);
-        errorHandler.addNotRetryableExceptions(
-                IllegalArgumentException.class
-        );
-        return errorHandler;
+        return new DefaultErrorHandler(recoverer, backOff);
     }
 }
